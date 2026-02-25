31c5cef80aa153f81e88849af79b36aeb5e6d000
Keyla Sisalima <keylasisalimaeaes@gmail.com>
2026-02-24 23:39:49 -0500

    Cambios realizados por Keyla

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
 })
-export class AppComponent {}
+export class AppComponent {
+
+  private router = inject(Router);
+
+  get esInicio(): boolean {
+    return this.router.url === '/' || this.router.url === '';
+  }
+
+}
